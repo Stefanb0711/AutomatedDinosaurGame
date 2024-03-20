3bee4e49db721166f3ec1927800413643e4c7ce7
Stefan <stefanboehme12@gmail.com>
2024-03-20 14:26:14 +0100

    Fertiges Spiel

---
+++ main.py
@@ -0,0 +1,21 @@
+import pyautogui
+import PIL
+from selenium import webdriver
+from selenium.webdriver import Keys
+from selenium.webdriver.common.by import By
+import time
+
+game_automater = DinoGameAutomater()
+
+game_automater.game()
+
+
+
+
+
+
+
+
+
+
+
